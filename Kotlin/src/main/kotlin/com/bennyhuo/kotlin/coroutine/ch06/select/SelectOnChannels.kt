51eeb6682fbc04d08066af220d86ab0703e79adf
bennyhuo <bennyhuo@kotliner.cn>
2020-03-01 08:10:32 +0800

    multiplex with flows.

--- Kotlin/src/main/kotlin/com/bennyhuo/kotlin/coroutine/ch06/select/SelectOnChannels.kt
+++ Kotlin/src/main/kotlin/com/bennyhuo/kotlin/coroutine/ch06/select/SelectOnChannels.kt
@@ -5,16 +5,20 @@ import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.actor
 import kotlinx.coroutines.channels.consumeEach
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.merge
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.selects.select
 import kotlin.random.Random
 
 suspend fun main() {
-    selectOnReceive()
+//    selectOnReceive()
 //    selectOnSend()
+    flowOnReceive()
 }
 
-suspend fun selectOnReceive(){
+suspend fun selectOnReceive() {
     val channels = List(10) { Channel<Int>() }
 
     GlobalScope.launch {
@@ -33,6 +37,31 @@ suspend fun selectOnReceive(){
             channel.onReceiveOrNull { it }
         }
     }
+
+    channels.map { it.consumeAsFlow() }.merge()
+        .first()
+
+    println(result)
+}
+
+suspend fun flowOnReceive() {
+    val channels = List(10) { Channel<Int>() }
+
+    GlobalScope.launch {
+        delay(100)
+        channels[Random.nextInt(10)].send(200)
+
+        channels.forEach {
+            it.close()
+        }
+    }
+
+    val result = channels.map {
+            it.consumeAsFlow()
+        }
+        .merge()
+        .first()
+
     println(result)
 }
 
--- Kotlin/src/main/kotlin/com/bennyhuo/kotlin/coroutine/ch06/select/Selects.kt
+++ Kotlin/src/main/kotlin/com/bennyhuo/kotlin/coroutine/ch06/select/Selects.kt
@@ -1,9 +1,14 @@
 package com.bennyhuo.kotlin.coroutine.ch06.select
 
+import com.bennyhuo.kotlin.coroutine.ch06.cancel.getUserSuspend
 import com.bennyhuo.kotlin.coroutine.common.api.User
 import com.bennyhuo.kotlin.coroutine.common.api.githubApi
 import com.google.gson.Gson
 import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.rx2.asSingle
+import kotlinx.coroutines.selects.SelectClause0
+import kotlinx.coroutines.selects.SelectInstance
 import kotlinx.coroutines.selects.select
 import java.io.File
 
@@ -11,21 +16,23 @@ val localDir = File("localCache").also { it.mkdirs() }
 
 val gson = Gson()
 
-fun CoroutineScope.getUserFromApi(login: String) = async(Dispatchers.IO){
+fun CoroutineScope.getUserFromApi(login: String) = async(Dispatchers.IO) {
     githubApi.getUserSuspend(login)
 }
 
-fun CoroutineScope.getUserFromLocal(login:String) = async(Dispatchers.IO){
-    File(localDir, login).takeIf { it.exists() }?.readText()?.let { gson.fromJson(it, User::class.java) }
+fun CoroutineScope.getUserFromLocal(login: String) = async(Dispatchers.IO) {
+    File(localDir, login).takeIf { it.exists() }?.readText()
+        ?.let { gson.fromJson(it, User::class.java) }
 }
 
-fun cacheUser(login: String, user: User){
+fun cacheUser(login: String, user: User) {
     File(localDir, login).writeText(gson.toJson(user))
 }
 
 data class Response<T>(val value: T, val isLocal: Boolean)
 
-suspend fun main() {
+
+suspend fun selectOnResponse() {
     val login = "bennyhuo"
     GlobalScope.launch {
         val localDeferred = getUserFromLocal(login)
@@ -43,4 +50,25 @@ suspend fun main() {
             println(userFromApi)
         }
     }.join()
+}
+
+suspend fun flowOnUser() {
+    coroutineScope {
+        val login = "bennyhuo"
+        listOf(::getUserFromApi, ::getUserFromLocal)
+            .map { function ->
+                function.call(login)
+            }
+            .map { deferred ->
+                flow { emit(deferred.await()) }
+            }
+            .merge()
+            .onEach { user ->
+                println("Result: $user")
+            }.launchIn(this)
+    }
+}
+
+suspend fun main() {
+    flowOnUser()
 }
